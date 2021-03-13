e5219a6ea0c65fc994d187ab74f40e7a021e579b
Kavya Shree <kavyashree@macusers-MacBook-Pro-4.local>
2021-03-13 16:16:25 +0530

    i am poor files added

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Kavya Shree on 13/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
